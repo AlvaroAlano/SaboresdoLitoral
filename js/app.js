c8aa1687e609acf4a10a1bd090e3c71e37de95c6
AlvaroAlano <122837277+AlvaroAlano@users.noreply.github.com>
2024-10-02 23:08:30 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -39,3 +39,45 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+window.addEventListener('resize', function() {
+  if (window.innerHeight < 500) {  // Defina o tamanho da janela ao detectar o teclado
+    document.querySelector('footer').style.display = 'none'; // Esconde o footer
+  } else {
+    document.querySelector('footer').style.display = 'flex'; // Mostra o footer novamente
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Função para aumentar ou diminuir a quantidade
+  const updateQuantity = (button, action) => {
+    const quantityElement = button.parentElement.querySelector('.quantity');
+    let currentQuantity = parseInt(quantityElement.textContent); // Converte o texto em número
+
+    if (action === 'increment') {
+      currentQuantity++; // Aumenta o valor
+    } else if (action === 'decrement' && currentQuantity > 0) {
+      currentQuantity--; // Diminui o valor, mas não pode ser menor que 0
+    }
+
+    quantityElement.textContent = `${currentQuantity} kg`; // Atualiza o texto da quantidade
+  };
+
+  // Seleciona todos os botões de incremento e decremento
+  const incrementButtons = document.querySelectorAll('.increment');
+  const decrementButtons = document.querySelectorAll('.decrement');
+
+  // Adiciona eventos de clique para os botões de incremento
+  incrementButtons.forEach(button => {
+    button.addEventListener('click', function() {
+      updateQuantity(button, 'increment');
+    });
+  });
+
+  // Adiciona eventos de clique para os botões de decremento
+  decrementButtons.forEach(button => {
+    button.addEventListener('click', function() {
+      updateQuantity(button, 'decrement');
+    });
+  });
+});
